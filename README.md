bef7a3496b16cff409a226b695b0e15beb40c8c3
Marco Schmalz <marco@schess.ch>
2017-06-29 16:22:54 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# Ein kleiner, fast fertiger Taschenrechner
+
+* Forkt dieses Repository (Fork-Knopf oben rechts)
+* Klont das *geforkte* Repository auf eurem lokalen Rechner. Im Fedora Terminal:
+```
+git clone ...
+```
+* Schreibt die euch zugewiesene Funktion in die Datei `taschenrechner.py`.
+* Testet ob die Funktion korrekt funktionert indem ihr das Programm ausführt:
+```
+python taschenrechner.py
+```
+* Speichert die Änderungen und kreiert einen Commit:
+```
+git add taschenrechner.py
+git commit -m "... was ihr gemacht habt"
+```
+* Veröffentlicht eure Änderungen auf GitHub (push):
+```
+git push origin master
+```
+* Kreiert eine Pull-Request hier auf GitHub.
---
+++ taschenrechner.py
@@ -0,0 +1,83 @@
+#!/usr/bin/python
+# encoding: utf-8
+from __future__ import print_function, division
+
+#---------------------
+def tokenize(program):
+    return program  # FIXME
+
+#---------------------
+def parse(tokens):
+    return tokens  # FIXME
+
+#---------------------
+def parse_atom(token):
+    return token  # FIXME
+
+#---------------------
+def add(a, b):
+    return a  # FIXME
+
+def sub(a, b):
+    return a  # FIXME
+    
+def mult(a, b):
+    return a  # FIXME
+
+def div(a, b):
+    return a  # FIXME
+
+operations = {
+    '+': add,
+    '-': sub,
+    '*': mult,
+    '/': div,
+}
+
+#---------------------
+def evaluate(x):
+    return x  # FIXME
+
+#=====================
+def repl():
+    while True:
+        try:
+            prog = raw_input('> ').strip()
+            if prog.lower() in ('q', 'quit', 'exit', 'x'):
+                break
+            print(evaluate(parse(tokenize(prog))))
+        except Exception as e:
+            print('Error')
+
+
+if __name__ == '__main__':
+    tests = [
+        (tokenize, ('(+ 1 1)',), ['(', '+', '1', '1', ')']),
+        (parse, (['(', '+', 1, 1, ')'],), ['+', 1, 1]),
+        (parse_atom, ('1.1',), 1.1),
+        (add, (1, 1), 2),
+        (sub, (2, 1), 1),
+        (mult, (2, 3), 6),
+        (div, (7, 2), 3.5),
+        (evaluate, (['*', ['+', 5, 9], ['-', 11, ['/', 128, 16]]],), 42)
+    ]
+    ok = True
+    for func, args, expected_out in tests:
+        try:
+            actual_out = func(*args)
+            if actual_out == expected_out:
+                print(func.__name__, 'OK')
+            else:
+                ok = False
+                print(func.__name__, 'not OK!', actual_out, '!=', expected_out)
+        except Exception as e:
+            ok = False
+            print(func.__name__, 'not OK!, Failure:', e)
+
+    if ok:
+        print('Alles OK!')
+        print()
+        print("Drücke 'q' um zu beenden..")
+        repl()
+    else:
+        print('Es scheint noch nicht alles ok zu sein. Korrigiere die oben angezeigten Fehler.')
